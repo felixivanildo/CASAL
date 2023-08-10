724f4411a153af4938f50739c1ebbd7b5615c504
Ivan Felix <felixsilva104@gmail.com>
2023-08-10 17:19:55 -0300

    Merge remote changes

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Ivan Felix',
+  email: 'ivanildo.felix@casal.al',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
